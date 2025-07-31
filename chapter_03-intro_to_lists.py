0cfddbb3c8ed13486498e8113090845a73999331
Espinoso <espinoso.ian@gmail.com>
2025-07-31 17:29:37 -0300

    Exercises in pages 77 and 78 partially done

--- chapter_03-intro_to_lists.py
+++ chapter_03-intro_to_lists.py
@@ -1,10 +1,22 @@
 # Create a list with the name of some people and print them
 names = ['arthur dent', 'zaphod beeblebrox', 'ford prefect']
 
-for name in names:
-    print(name)
+# for name in names:
+#     print(name)
 
-# Print a personalized message for them
-message = "I love you, "
+# # Print a personalized message for them
+# message = "I love you, "
+# for name in names:
+#     print(f"{message} {name.title()}")
+
+# for name in names:
+#     print(f"{name.title()}, you're invited to the party!")
+
+substitutes = ['jane goodall', 'ana primavesi', 'audre lorde']
+unavailable = []
+
 for name in names:
-    print(f"{message} {name.title()}")
+    unavailable.append(names.pop())
+  # print(f"Saddly, {names.title()} won't be able to attend.\nWelcome to the party, {substitute[-1].title()}")
+
+# Reinvite the people into the party
