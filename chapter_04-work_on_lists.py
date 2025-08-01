ee244b8f7ccd1bd35eb3b2e73a025ffcd1ceeb9d
Espinoso <espinoso.ian@gmail.com>
2025-08-01 16:56:29 -0300

    Finnished exercises from page 92

---
+++ chapter_04-work_on_lists.py
@@ -0,0 +1,17 @@
+# Generate a list of my favorit sandwitches
+sandwitches = ['bauru', 'croque madame', 'choripan']
+
+# Print a sentence saying that I like them
+for sandwitch in sandwitches:
+    print(f"I could eat a {sandwitch} anytime.\n")
+
+print("I love sandwitches!\n\n")
+
+# Generate a list of animals with similar characteristics
+animals = ['crab', 'vinegar-scorpion', 'armadillo']
+
+# Print some sentences about them
+for animal in animals:
+    print(f"The {animal} has an exoescheleton.\n")
+
+print("With that, they support the carcinotropic theory.\n\n")
