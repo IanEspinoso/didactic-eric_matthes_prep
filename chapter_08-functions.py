5b672d538d79da249f27cf862b6c69a7be96055d
Espinoso <espinoso.ian@gmail.com>
2025-08-06 13:04:34 -0300

    Finis exercises in page 176

---
+++ chapter_08-functions.py
@@ -0,0 +1,15 @@
+## Create a function called display_message() that tells what I'm learning
+
+def display_message():
+    """Display a message telling what I'm learning"""
+    print("I'm learning how to code functions!")
+
+display_message()
+
+
+## Write  a function called favorite_book(), with a parameter and a message
+
+def favorite_book(title):
+    print(f"My favorite book is {title.title()}.")
+
+favorite_book('the fellowship of the ring')
