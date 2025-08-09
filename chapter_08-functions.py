ff4744dfc5768341bd000a1189b1444427e0d16c
Espinoso <espinoso.ian@gmail.com>
2025-08-08 21:47:41 -0300

    Finish exercises from page 203, thus chapter 8

--- chapter_08-functions.py
+++ chapter_08-functions.py
@@ -1,10 +1,37 @@
 ## Rename the original document of chapter 8 as printing_models.py
 ## Then import the functions within it with the following methods
 # import name_mdl
+
+import printing_models
+
+printing_models.display_message()
+
+
 # from name_mdl import name_fnct
+
+from printing_models import favorite_book
+
+favorite_book('o menino maluquinho')
+
+
 # from name_mdl import name_fnct as nf
+
+from printing_models import make_shirt as ms
+
+ms('tiny', "this is my friday's shirt")
+
+
 # import name_mdl as nm
+
+import printing_models as pm
+
+pm.describe_city('auckland', 'new zealand')
+
+
 # from name_mdl import *
 
+from printing_models import *
 
-
+is_this_real = get_plant_info('psychotria' ,'viridis', 
+                              popular_name='chacrona', origin='amazon')
+print(is_this_real)
