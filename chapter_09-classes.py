767e64b7eafb76ccde0bb26c0c4e6d7abd9141a9
Espinoso <espinoso.ian@gmail.com>
2025-08-09 11:27:12 -0300

    Finish exercises of pages 210 and 211

---
+++ chapter_09-classes.py
@@ -0,0 +1,84 @@
+## Create a class callde Restaurant, receiving two attributes in its init
+
+class Restaurant:
+    """My first class: a restaurant synthetizer."""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        """Initializes name and type attributes."""
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+## Create two functions within it, calling its attributes
+
+    def describe_restaurant(self):
+        """Describes the restaurant, using the given attributes."""
+        print(f"{self.restaurant_name.title()} specialty is "\
+              f"{self.cuisine_type} cuisine.")
+
+    def open_restaurant(self):
+        """Tells if the restaurant is open or not"""
+        print("The restaurant is open.")
+
+##Instantiate it and call it for testing
+
+restaurant = Restaurant('the bear', 'sbasguetti')
+
+restaurant.describe_restaurant()
+
+restaurant.open_restaurant()
+
+## Create three more instances of it, and call 'describe restaurant' for them
+
+restaurant_1 = Restaurant('habibs', 'bad arab')
+
+restaurant_2 = Restaurant('macdonalds', 'trash')
+
+restaurant_3 = Restaurant('my mom', 'the best')
+
+restaurant_1.describe_restaurant()
+
+restaurant_2.describe_restaurant()
+
+restaurant_3.describe_restaurant()
+
+## Create a class called User using two attributes and a kwargs
+
+class User:
+    """Store and display user profile information."""
+
+    def __init__(self, first_name, last_name, **kwargs):
+        """Initializes the user's attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.kwargs = kwargs
+  
+    def describe_user(self):
+        """Simlpy displays the given attributes."""
+        print(f"User name is {self.first_name.title()}"\
+              f" {self.last_name.title()}")
+        if self.kwargs:
+            print("User attributes:")
+            for key, value in self.kwargs.items():
+                print(key, ":", value)
+
+    def greet_user(self):
+        """Send a personalized greeting message to the user."""
+        print(f"Welcome back, {self.first_name.title()} {self.last_name.title()}!")
+
+## Create three instances of it and call both methods
+
+user_0 = User('ian', 'espinoso')
+
+user_1 = User('gandalf', 'the grey', daddy='eru iluvatar')
+
+user_2 = User('escherichia', 'coli', 
+              hometown='intestinal tract', reign='bacteria')
+
+user_0.describe_user()
+user_0.greet_user()
+
+user_1.describe_user()
+user_1.greet_user()
+
+user_2.describe_user()
+user_2.greet_user()
