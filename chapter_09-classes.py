c224cb6bc76b0e1dd2af12ad977a1fbe73d9c042
Espinoso <espinoso.ian@gmail.com>
2025-08-13 19:04:45 -0300

    Remove the Car class from the chapter's main code, and import it separately

--- chapter_09-classes.py
+++ chapter_09-classes.py
@@ -201,35 +201,7 @@ the_boss.show_privileges()
 
 ## Code an electric car.
 
-class Car:
-    """Simple attempt to representing a car."""
-
-    def __init__(self, make, model, year):
-        """Initialize the attributes to describe a car."""
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-
-    def get_descriptive_name(self):
-        """Returns a descriptive name, elegantly formated."""
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
-    
-    def read_odometer(self):
-        """Displays a sentence showing the the car's mileage."""
-        print(f"This car has {self.odemeter_reading} miles on it.")
-
-    def update_odometer(self, mileage):
-        """Redefines the odometer reading to the given value."""
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't rol back an odometer!")
-
-    def increment_odometer(self, miles):
-        """Adds a given amount to the odometer reading."""
-        self.odometer_reading += miles
+from car import Car
 
 class Battery:
     """Simple attepmt to model an electric car battery."""
@@ -284,3 +256,4 @@ print('Now updating...')
 my_green_car.upgrade_battery()
 
 my_green_car.battery.get_range()
+
