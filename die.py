9c984950029f83cf5b620e7edd4d4817b0a0e4d0
Espinoso <espinoso.ian@gmail.com>
2025-08-14 00:15:57 -0300

    Troublehsoot die.py to remove importing problems from external package

--- die.py
+++ die.py
@@ -1,13 +1,14 @@
 """Simple class attempting to represent dice."""
 
+
 class Die:
     """Simple class attempting to represent dice."""
-    from random import randint
-
+    
     def __init__(self, sides):
         """Initializes the class attributes."""
         self.sides = sides
 
     def roll_die(self):
         """Rolls a random number, depending on the number of sides."""
+        from random import randint
         return randint(1, self.sides)
