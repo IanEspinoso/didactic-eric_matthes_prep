a41de5f5eb33981e22b6f63f957c5cfe1677dde3
Espinoso <espinoso.ian@gmail.com>
2025-08-27 15:56:33 -0300

    Finish exercises from page 243

---
+++ chapter_10-exceptions.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+
+## Exercise 10.1
+
+path = Path('learning_python.txt')
+contents = path.read_text()
+"print(contents)" # First, direct reading
+
+lines = contents.splitlines()
+"""for line in lines: # Second, reading line by line
+    print(line)"""
+
+
+## Exercise 10.2
+
+"print(contents.replace('Python', 'C'))"
+
+
+## Exercise 10.3
+
+"""for line in contents.splitlines():
+    print(line)"""
+
+
+## Exercise 10.4
+
+
+
+
+## Exercise 10.5
+
+
+
+
+## Exercise 10.6
+
+
+
+
+## Exercise 10.7
+
+
+
+
+## Exercise 10.8
+
+
+
+
+## Exercise 10.9
+
+
+
+
+## Exercise 10.10
+
+
+
+
+## Exercise 10.11
+
+
+
+
+## Exercise 10.12
+
+
+
+
+## Exercise 10.13
+
+
+
+
+## Exercise 10.14
+
+
+
