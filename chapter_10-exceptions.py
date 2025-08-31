13f3ddde4722cd332e25fdf5eef57d163f693df2
Espinoso <espinoso.ian@gmail.com>
2025-08-30 23:04:28 -0300

    Exercises of page 255 solved: 4 out of 5

--- chapter_10-exceptions.py
+++ chapter_10-exceptions.py
@@ -46,28 +46,92 @@ guests.read_text()"""
 
 ## Exercise 10.6
 
-
+"""try:
+    n1 = input("Enter the first number")
+    n1 = int(n1)
+    n2 = input("Enter the second number")
+    n2 = int(n2)
+    print(f"The sum is {n1 + n2}")
+except ValueError:
+    print("The numbers should be integers, not strings.")"""
 
 
 ## Exercise 10.7
 
+"""n1, n2 = 0, 0
 
+while True:
+    try:
+        n1 = input("Enter the first number ('q' to quit)")
+        if n1 == 'q':
+            break
+        n1 = int(n1)
+        n2 = input("Enter the second number ('q' to quit)")
+        if n2 == 'q':
+            break
+        n2 = int(n2)
+        print(f"The sum is {n1 + n2}")
+    except ValueError:
+        print("The numbers should be integers, not strings.")"""
 
 
 ## Exercise 10.8
 
+"""try:
+    print("\nThis are the cats:")
+    for cat in Path('cats.txt').read_text().splitlines():
+        print(cat)
+    print("\nThis are the dogs:")
+    for dog in Path('dogs.txt').read_text().splitlines():
+        print(dog)
+except FileNotFoundError:
+    print("File Not Found: document does not exists or is elsewhere.")"""
 
 
 
 ## Exercise 10.9
 
-
+"""try:
+    cats =  Path('cats.txt').read_text().splitlines()
+    print("\nThis are the cats:")
+    for cat in cats:
+        print(cat)
+    dogs = Path('dogs.txt').read_text().splitlines()
+    print("\nThis are the dogs:")
+    for dog in dogs:
+        print(dog)
+except FileNotFoundError:
+    pass"""
 
 
 ## Exercise 10.10
 
+def most_frequent_word(path):
+    """Returns the word with the highest count in a txt."""
+   
+    try:
+        text = Path(path).read_text(encoding='utf-8')
+    except FileNotFoundError:
+        print(f"Sorry, {path} could not be found.")
+    else:
+        words = text.split()
+        lower_words = [word.lower() for word in words]
+        unique_words = set(lower_words)
+        occurrence_count = dict.fromkeys(unique_words)
+        for key in occurrence_count:
+            occurrence_count[key] = text.count(key)
+        max_value = max(occurrence_count.values())
+        for key, value in occurrence_count.items():
+            if max_value == value:
+                max_key = key
 
+    max_dict = {max_key: max_value}
+    
+    return max_dict
 
+moby_dick_word = most_frequent_word('moby_dick.txt')
+
+print('helo world')
 
 ## Exercise 10.11
 
