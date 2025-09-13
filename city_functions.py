af07f052c1eeb8a3537bec43ddb127e85ca50bde
Espinoso <espinoso.ian@gmail.com>
2025-09-13 19:29:01 -0300

    Finish second exercise from chapter 11

--- city_functions.py
+++ city_functions.py
@@ -1,4 +1,6 @@
-def city_country(city, country):
-    """Pretty prints a given 'City, Country'."""
+def city_country(city, country, population=''):
+    """Pretty prints a given city, country and population."""
 
-    return f"{city.capitalize()}, {country.capitalize()}"
+    if population != '':
+        population = f" - population {population}"
+    return f"{city.capitalize()}, {country.capitalize()}{population}"
--- test_cities.py
+++ test_cities.py
@@ -1,7 +1,13 @@
 from city_functions import city_country
 
 def test_city_country():
-    """Test city_functions."""
+    """Test city functions without 'population'."""
 
     city_country_1 = city_country('santiago', 'chile')
     assert city_country_1 == 'Santiago, Chile'
+
+def test_city_country_population():
+    """Test city functions with all the arguments."""
+
+    city_country_1 = city_country('santiago', 'chile', population='5000000')
+    assert city_country_1 == 'Santiago, Chile - population 5000000'
